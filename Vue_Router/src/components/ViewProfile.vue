16be3e678a486b9367bc1109cfccfe2a09a14a91
Sameer Khan <sameer_khan@student.uml.edu>
2018-07-04 16:21:00 -0400

    Vue router basics completed

---
+++ Vue_Router/src/components/ViewProfile.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="view-profile">
+    <h2>Profile for {{ userID }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ViewProfile',
+  data() {
+    return {
+      userID: this.$route.params.user_id,
+    };
+  },
+  methods: {
+    updateId() {
+      this.userID = this.$route.params.user_id;
+    },
+  },
+  watch: {
+    $route: 'updateId',
+  },
+};
+</script>
+
+<style scoped>
+</style>
